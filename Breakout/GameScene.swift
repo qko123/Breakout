1da0b4197f18420ab47b466789ebce69c42ac165
Quinn Kozak <quinn.kozak@icloud.com>
2023-03-09 09:43:47 -0600

    Determine winners and losers

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -101,4 +101,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             paddle.position.x = location.x
         }
     }
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "brick" ||
+            contact.bodyB.node?.name == "brick" {
+            print("You Win!")
+            brick.removeFromParent()
+            ball.removeFromParent()
+        }
+        if contact.bodyA.node?.name == "loseZone" ||
+            contact.bodyB.node?.name == "loseZone" {
+            print("You Lose!")
+            ball.removeFromParent()
+        }
+    }
 }
